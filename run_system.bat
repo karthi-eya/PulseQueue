351fd12ee9e2bf4711002e4ac85ca3541381d847
Karthikeya <duddekarthikeya@gmail.com>
2026-03-24 19:20:12 +0530

    small

--- run_system.bat
+++ run_system.bat
@@ -3,3 +3,4 @@ start cmd /k uvicorn backend.patient_backend:app --port 8001 --reload
 start cmd /k uvicorn backend.receptionist_backend:app --port 8002 --reload
 start cmd /k uvicorn backend.doctor_backend:app --port 8003 --reload
 start cmd /k cd frontend && python -m http.server 5500
+start cmd /k cd frontend/doctor && npm run dev
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\KARTHIKEYA\OneDrive\Desktop\GitHub\webdev_lab1\PulseQueue\venv
+set VIRTUAL_ENV=C:\GitHub\webdev_lab1\PulseQueue\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
